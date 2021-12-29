01956e91d1b7c447ab97bd78435449daa8a3fb47
Dmitrii Pichulin <deem@deem.ru>
2021-12-29 19:08:43 +0300

    up

--- updater.sh
+++ updater.sh
@@ -3,4 +3,6 @@
 cd $(dirname $0)
 unlink var/db/scams.txt
 unlink var/db/weights.txt
+unlink var/db/blockchain.sqlite3-wal
+unlink var/db/blockchain.sqlite3-shm
 while :; do echo '' && date "+%Y.%m.%d %H:%M:%S" && echo "-------------------"; php w8_updater.php; echo "-------------------"; date "+%Y.%m.%d %H:%M:%S"; sleep 17; done
