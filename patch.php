7b082ed44da75b183c6ec3a9bdc899307cdb0f36
Dmitrii Pichulin <deem@deem.ru>
2023-11-14 15:11:32 +0300

    patch for data table with r7 (puid)

--- patch.php
+++ patch.php
@@ -7,11 +7,29 @@ if( file_exists( 'config.php' ) )
 else
     require_once 'config.sample.php';
 
-$db = new \deemru\Triples( W8DB, 'data' );
-
-$patches = $db->query( 'SELECT r0 FROM data WHERE r6 = 0' );
-
-foreach( $patches as $patch )
+if( 0 )
 {
-    $db->query( 'UPDATE data SET r2 = 0 WHERE r0 = ' . $patch[0] );
+    $db = new \deemru\Triples( W8DB, 'data' );
+    $patches = $db->query( 'SELECT r0 FROM data WHERE r6 = 0' );
+    foreach( $patches as $patch )
+    {
+        $db->query( 'UPDATE data SET r2 = 0 WHERE r0 = ' . $patch[0] );
+    }
+}
+
+if( 10 )
+{
+    $cmds = 
+    [
+        'ALTER TABLE data DROP COLUMN r7',
+    ];
+
+    foreach( $cmds as $cmd )
+    {
+        $tt = microtime( true );
+        $cmd = 'sqlite3 ' . W8IO_DB_PATH . ' "' . $cmd . '"';
+        wk()->log( 'exec( ' . $cmd . ' )' );
+        exec( $cmd );
+        wk()->log( sprintf( '%.00f seconds', ( microtime( true ) - $tt ) ) );
+    }
 }
