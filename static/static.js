90d8f6dc8f27fb92a91afc71b4a1eabcfb89c7a8
Dmitrii Pichulin <deem@deem.ru>
2025-08-21 15:23:36 +0300

    update lazyload with lazytick

--- static/static.js
+++ static/static.js
@@ -1,22 +1,22 @@
+var g_loading = false;
+var g_lazyload = null;
+var g_lazytick = null;
+
 $(document).ready( function()
 {
-    console.log( document.cookie )
     var z = new Date().getTimezoneOffset();
-    if( z % 1 === 0 )
+    var cookie = "z=" + ( -z ) + "m";
+    if( document.cookie.indexOf( cookie ) === -1 )
     {
-        var cookie = "z=" + ( -z ) + "m";
-        if( document.cookie.indexOf( cookie ) === -1 )
-        {
-            document.cookie = cookie + "; max-age=31536000; path=/";
-            document.location.replace( document.location.href );
-        }
+        document.cookie = cookie + "; max-age=31536000; path=/";
+        document.location.replace( document.location.href );
     }
 
     g_loading = false;
     g_lazyload = $(".lazyload");
     if( g_lazyload.length )
     {
-        $(window).scroll( lazyload );
+        $(window).scroll( lazytick );
         lazyload();
     }
 
@@ -32,35 +32,74 @@ $(document).ready( function()
     } );
 } );
 
+function lazytick()
+{
+    if( g_lazytick )
+        clearTimeout( g_lazytick );
+    g_lazytick = setTimeout( function(){ lazyload(); }, 100 );
+}
+
 function lazyload()
 {
-    if( g_loading )
+    if( g_loading || !g_lazyload || !g_lazyload.length )
         return;
 
-    var wt = typeof this.scrollY !== "undefined" ? this.scrollY : $(window).scrollTop();
-    var wb = wt + this.innerHeight;
+    var wt = ( typeof window.scrollY !== "undefined" ) ? window.scrollY : $(window).scrollTop();
+    var wb = wt + window.innerHeight;
+
+    if( !g_lazyload.offset )
+    {
+        g_lazyload = $(".lazyload");
+        if( !g_lazyload.length )
+        {
+            $(window).off( "scroll", lazytick );
+            return;
+        }
+    }
+
     var ot = g_lazyload.offset().top;
     var ob = ot + g_lazyload.height();
 
     if( wt <= ob && wb >= ot )
     {
         g_loading = true;
-        $.get( g_lazyload.attr( "url" ), null, function( data )
-        {
-            if( data )
-            {
-                $( data ).insertAfter( g_lazyload );
-                g_lazyload.remove();
 
-                g_lazyload = $(".lazyload");
-                if( g_lazyload.length )
+        var url = g_lazyload.attr( "url" );
+        if( !url )
+        {
+            console.error( "lazyload missing 'url'" );
+            g_loading = false;
+            $(window).off( "scroll", lazytick );
+            return;
+        }
+
+        $.get( url )
+            .done( function( data )
+            {
+                if( data )
+                {
+                    $( data ).insertAfter( g_lazyload );
+                    g_lazyload.remove();
+
+                    g_lazyload = $(".lazyload");
+                    if( g_lazyload.length )
+                    {
+                        g_loading = false;
+                        return lazyload();
+                    }
+                }
+
+                g_loading = false;
+                $(window).off( "scroll", lazytick );
+            } )
+            .fail( function( xhr, status, error )
+            {
+                console.error( "lazyload request failed: " + status + " (" + error + ") - retrying in 8s" );
+                setTimeout( function()
                 {
                     g_loading = false;
-                    return lazyload();
-                }
-            }
-
-            $(window).off( "scroll", lazyload );
-        } );
+                    lazyload();
+                }, 8000 );
+            } );
     }
 }
